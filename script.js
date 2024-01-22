199df5a37ac0718ef90b4356d995bcabd56537a2
Vertine1 <verthennys@gmail.com>
2024-01-22 21:53:44 +0300

    Deleted some script they were repeating themselves

--- script.js
+++ script.js
@@ -19,32 +19,3 @@ function game(){
     
 }
 }
-
-
-const btn = document.querySelector("button");
-const canvas = document.querySelector("canvas");
-const ctx = canvas.getContext("2d");
-
-document.addEventListener("DOMContentLoaded", () => {
-  canvas.width = document.documentElement.clientWidth;
-});
-
-function random(number) {
-  return Math.floor(Math.random() * number);
-}
-
-function draw() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  for (let i = 0; i < 100; i++) {
-    ctx.beginPath();
-    ctx.fillStyle = "rgb(255 0 0 / 50%)";
-    ctx.arc(
-      random(canvas.width),
-      random(canvas.height),
-      random(50),
-      0,
-      2 * Math.PI,
-    );
-    ctx.fill();
-  }
-}
