68436f329818b17b1a4ad0aec2ef06b163b020c5
Vertine1 <verthennys@gmail.com>
2024-01-24 13:47:24 +0300

    added variables 'playerScore' & 'computerScore

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let playerScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
     let choices = ['Rock', 'Paper', 'Scissors'];
     return choices[Math.floor(Math.random() * choices.length)];
@@ -27,6 +30,35 @@ function game(playerSelection) {
     const computerSelection = getComputerChoice();
     const result = playRound(playerSelection, computerSelection);
     updateResultDisplay(result);
+
+    // THis is display running score
+    const scoreDisplay =document.getElementById('scoreDisplay');
+    scoreDisplay.textContent= 'Player: ${playerScore} | ComputerScore}';
+
+    // this statements righ here checks for a winner
+    if (playerScore === 5 || computerScore === 5) {
+        announceWinner();
+    }
+}
+
+function announceWinner(){
+    const winnerDisplay = document.getElementById('winnerDisplay');
+    if (playerScore === 5) {
+        winnerDisplay.textContent = 'Congratulations! You are the winner Champ!';
+
+    } else {
+        winnerDisplay.textContent = 'oooh No you Lost .looser. Better luck next round!';
+
+    }
+
+    // This is where the game reset scores for a new game
+    playerScore = 0;
+    computerScore = 0;
+}
+function game(playerSelection){
+    const computerSelection =getComputerChoice();
+const result = playRound(playerSelection, computerSelection);
+updateResultDisplay(result);
 }
 
 document.getElementById('rockButton').addEventListener('click', function () {
